c42b6f272846a0f865fcbf31a4d9d1b108541ecf
jinhyun <free1330@gmail.com>
2015-08-07 23:45:04 +0900

    feature(calc): Plus 구현

--- src/js/calcMain.js
+++ src/js/calcMain.js
@@ -1,17 +1,41 @@
-var calcMain = {};
+var calcMain = {
+    btnFuncObj : ''
+};
 
 calcMain.init = function () {
+    calcMain.btnFuncObj = {
+        turnOn: false,
+        funcName: '',
+        srcContents: ''
+    };
+
     calcMain.bind();
+};
+
+calcMain.init.btnFuncObjInit = function () {
+    calcMain.btnFuncObj = {
+        turnOn: false,
+        funcName: '',
+        srcContents: ''
+    };
 };
 
 calcMain.bind = function () {
     var btnNum = function () {
         var btnNumsElemName = 'btn_num';
         var btnNums = document.getElementsByName(btnNumsElemName);
+        var btnFuncObj;
+        var srcContents;
 
         for(var i = btnNums.length; i--;){
             btnNums[i].onclick = function () {
-                var srcContents = calcMain.getCalcDisplay();
+                btnFuncObj = calcMain.btnFuncObj;
+                if (btnFuncObj.turnOn) {
+                    srcContents = '0';
+                } else {
+                    srcContents = calcMain.getCalcDisplay();
+                }
+
                 var contents = (srcContents == 0) ? this.value : srcContents + this.value;
 
                 calcMain.setCalcDisplay(contents);
@@ -27,10 +51,33 @@ calcMain.bind = function () {
             btnFuncs[i].onclick = function () {
                 var func = this.id.replace(btnFuncElemName + '_', '');
                 var contents;
+                var btnFuncObj = calcMain.btnFuncObj;
+                var srcContents;
+                var destContents;
 
                 switch (func){
                     case 'clear':
                         contents = '0';
+                        calcMain.init.btnFuncObjInit();
+                        break;
+                    case 'plus':
+                        // Value assigned to primitive will be lost
+                        btnFuncObj.funcName = 'plus';
+                        btnFuncObj.turnOn = true;
+                        btnFuncObj.srcContents = calcMain.getCalcDisplay();
+
+                        contents = calcMain.getCalcDisplay();
+                        break;
+                    case 'equal':
+                        switch (btnFuncObj.funcName){
+                            case 'plus':
+                                srcContents = btnFuncObj.srcContents;
+                                destContents = calcMain.getCalcDisplay();
+                                contents = Number(srcContents) + Number(destContents);
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     default:
                         contents = calcMain.getCalcDisplay();
